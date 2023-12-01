53847da6d59d9f88767ad5bb9c4030a692504481
Jason C <grousemusic@gmail.com>
2023-12-01 12:45:22 -0500

    Creando componentes y configurando rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, SharedModule, AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
