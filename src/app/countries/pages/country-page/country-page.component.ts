2e305e8d07250cede760717dc449821c350ff4fc
Jason C <grousemusic@gmail.com>
2023-12-02 19:11:19 -0500

    Argumentos por URL - Página de País

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private countriesService: CountriesService
+  ) {}
 
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(({ id }) => {
+      this.countriesService
+        .searchCountryByAlphaCode(id)
+        .subscribe((country) => {});
+    });
+  }
 }
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -2,12 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, catchError, of } from 'rxjs';
 import { Country } from '../interfaces/country';
+import { SearchBoxComponent } from '../../shared/components/search-box/search-box.component';
 
 @Injectable({ providedIn: 'root' })
 export class CountriesService {
   private apiUrl: string = 'https://restcountries.com/v3.1';
 
   constructor(private http: HttpClient) {}
+
+  searchCountryByAlphaCode(code: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/alpha/${code}`;
+    return this.http.get<Country[]>(url).pipe(catchError(() => of([])));
+  }
 
   searchCapital(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${term}`;
