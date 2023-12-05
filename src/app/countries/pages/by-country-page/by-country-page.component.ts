6b7024a0a25ec36a9e5c28db6f86391f4c374ed7
Jason C <grousemusic@gmail.com>
2023-12-05 18:12:20 -0500

    implementando     <shared-loading-spinner >

--- src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -11,12 +11,18 @@ import { Country } from '../../interfaces/country';
 export class ByCountryPageComponent {
 
   public countries: Country[] = [];
+  public isLoading: boolean = false;
+
 
   constructor(private countriesService: CountriesService) {}
 
   searchByCountry(term: string): void {
+    this.isLoading = true;
+
     this.countriesService.searchCountry(term).subscribe(countries => {
       this.countries = countries;
+      this.isLoading = false;
+
     });
   }
 }
--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -9,12 +9,19 @@ import { Country } from '../../interfaces/country';
 })
 export class ByRegionPageComponent {
   public countries: Country[] = [];
+  public isLoading: boolean = false;
+
+
 
   constructor(private countriesService: CountriesService) {}
 
   searchByRegion(region: string): void {
+    this.isLoading = true;
+
     this.countriesService.searchRegion(region).subscribe((countries) => {
       this.countries = countries;
+      this.isLoading = false;
+
     });
   }
 }
