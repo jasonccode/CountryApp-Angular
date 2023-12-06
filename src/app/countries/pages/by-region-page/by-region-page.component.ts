274001f467503f3224c5d57c82a1e23812c65799
Jason C <grousemusic@gmail.com>
2023-12-06 18:11:59 -0500

    Buscar por region - Optimización

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -10,18 +12,25 @@ import { Country } from '../../interfaces/country';
 export class ByRegionPageComponent {
   public countries: Country[] = [];
   public isLoading: boolean = false;
-
-
+  public regions: Region[] = [
+    'Africa',
+    'Americas',
+    'Asia',
+    'Europe',
+    'Oceania',
+  ];
+  public selectedRegion?: Region;
 
   constructor(private countriesService: CountriesService) {}
 
-  searchByRegion(region: string): void {
+  searchByRegion(region: Region): void {
+    this.selectedRegion = region;
+
     this.isLoading = true;
 
     this.countriesService.searchRegion(region).subscribe((countries) => {
       this.countries = countries;
       this.isLoading = false;
-
     });
   }
 }
